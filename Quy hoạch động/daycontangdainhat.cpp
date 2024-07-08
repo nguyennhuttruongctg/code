c4d67a4f46712e9e2208ce695152d8572e6eb5d4
Nguyễn Nhựt Trường <142295331+nhuttruong23@users.noreply.github.com>
2024-07-09 00:23:03 +0700

    Create daycontangdainhat.cpp

---
+++ Quy hoạch động/daycontangdainhat.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define ld long double
+#define fi first
+#define se second
+#define pb push_back
+#define len(s) (int)s.size()
+#define OpenFile(Name) freopen(Name".inp","r",stdin); freopen(Name".out","w",stdout);
+using namespace std;
+const int lim=1e6+5,M2C=1e3,MOD=1e9+7;
+
+int a[lim],l[lim];
+
+int main(){
+    ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+    #ifndef ONLINE_JUDGE
+        OpenFile("task");
+    #endif
+
+    int n,res=0; cin>>n;
+    for (int i=1;i<=n;i++) cin>>a[i];
+
+    for (int i=1;i<=n;i++) {
+        l[i]=1;
+        for (int j=1;j<i;j++)
+        if (a[i]>a[j] && l[j]+1>l[i]) l[i]=l[j]+1;
+        res=max(res,l[i]);
+    }
+    cout<<res;
+
+    return 0;
+}
