d8133fd654084cb113987e203b59b526d9319960
Nguyễn Nhựt Trường <142295331+nguyennhuttruongctg@users.noreply.github.com>
2024-08-06 22:30:47 +0700

    Create sinhtohop.cpp

---
+++ Sinh/sinhtohop.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define fi first
+#define se second
+#define pb push_back
+#define len(s) (int)s.size()
+#define OpenFile(Name) freopen(Name".inp","r",stdin); //freopen(Name".out","w",stdout);
+using namespace std;
+const int lim=1e6+4,M2C=1e3,MOD=1e9+7;
+
+int x[20],n,k;
+bool danhdau[20];
+
+void print(){
+	for (int i=1;i<=k;i++) cout<<x[i]<<' ';
+	cout<<'\n';
+}
+
+void Try(int i){
+	for (int j=x[i-1]+1;j<=n-k+i;j++) {
+		x[i]=j;
+		if (i==k) print(); else Try(i+1);
+	}	
+}
+
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+    #ifndef ONLINE_JUDGE
+        OpenFile("task");
+    #endif
+   	
+   	cin>>n>>k;
+	Try(1);
+   	
+	return 0;
+}
